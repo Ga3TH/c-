35a10f2637f8e660e78ddfe215f1f3e0fb812b30
gaeth-hub <gaeth0311@gmail.com>
2026-01-26 12:14:05 +0100

    prout

--- Matrice/ClasseMatrice.cs
+++ Matrice/ClasseMatrice.cs
@@ -8,6 +8,51 @@ namespace Matrice
 {
     internal class ClasseMatrice
     {
-        int 
-    }
+        int[,] coefficients;
+        int ordre;
+
+        public ClasseMatrice(int pOrdre, int pMin, int pMax)
+        {
+            Random rand = new Random();
+            int ordre = pOrdre;
+            for (int i = 0; i <= ordre; i++)
+            {
+                for (int j = 0; j <= ordre; j++)
+                {
+                    coefficients[i,j] = rand.Next(pMin, pMax);
+                }
+            }
+        }
+        public ClasseMatrice(int[,] pTableau)
+        {
+            coefficients = pTableau;
+        }
+        public ClasseMatrice MultiplierParScalaire(int scalaire)
+        {
+            int[,] tabScalaire = new int[ordre, ordre];
+            for (int i = 0; i < ordre; i++) 
+            { 
+                for (int j = 0;j < ordre; j++) 
+                {
+                    tabScalaire[i,j] = coefficients[i,j] * scalaire;
+                    
+                }
+            }
+            
+            return new ClasseMatrice(tabScalaire);
+        }
+        public ClasseMatrice Addition(ClasseMatrice m)
+        {
+
+            int[,] tabScalaire = new int[ordre, ordre];
+            for (int i = 0; i < ordre; i++)
+            {
+                for (int j = 0; j < ordre; j++)
+                {
+                    tabScalaire[i, j] = coefficients[i, j] + m;
+                }
+            }
+            return new ClasseMatrice(tabScalaire);
+        }
+    }   
 }
